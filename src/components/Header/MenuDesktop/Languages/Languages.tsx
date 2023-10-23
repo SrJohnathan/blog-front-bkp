67187f078de8e6f0d864d68757802255ea682fc8
DayaneArnaud <dayanearnaud@gmail.com>
2023-10-23 11:18:06 -0300

    creating branch develop

--- src/components/Header/MenuDesktop/Languages/Languages.tsx
+++ src/components/Header/MenuDesktop/Languages/Languages.tsx
@@ -1,43 +1,44 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Languages = () => {
   return (
     <button className="circle transparent">
-      <i>translate</i>
+      {/* <i>translate</i>
       <menu className="no-wrap left">
-        <a className="row">
+        <Link className="row">
           <Image
             className="circle tiny"
             src="/band/usa.svg"
             alt="Change to English language"
           />
           <span>English</span>
-        </a>
-        <a className="row">
+        </Link>
+        <Link className="row">
           <Image
             className="circle tiny"
             src="/band/france.svg"
             alt="Changer en langue française"
           />
           <span>Français</span>
-        </a>
-        <a className="row">
+        </Link>
+        <Link className="row">
           <Image
             className="circle tiny"
             src="/band/spain.svg"
             alt="Cambiar para idioma Español"
           />
           <span>Español</span>
-        </a>
-        <a className="row">
+        </Link>
+        <Link className="row">
           <Image
             className="circle tiny"
             src="/band/portugal.svg"
             alt="Mudar para o idioma Português"
           />
           <span>Portuguese</span>
-        </a>
-      </menu>
+        </Link>
+      </menu> */}
     </button>
   );
 };
