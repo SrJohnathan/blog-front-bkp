a719bfdef101f85e0864c31061d0cbddea52e38b
DayaneArnaud <dayanearnaud@gmail.com>
2023-10-30 14:50:50 -0300

    [FEAT]: Working on header mobile menu dropdown

--- src/components/Header/MenuMobile/MenuMobile.tsx
+++ src/components/Header/MenuMobile/MenuMobile.tsx
@@ -4,10 +4,57 @@ import React, { useContext, useState } from "react";
 import { ThemeModeContext } from "@/context/ThemeModeContext";
 import Image from "next/image";
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 export const MenuMobile = () => {
+  const t = useTranslations("NavLinkDesktopFooter");
   const { theme, toggleTheme } = useContext(ThemeModeContext);
   const [drawer, setDrawer] = useState(false);
+  const [selectedTopic, setSelectedTopic] = useState<string | null>(null);
+
+  const topics = [
+    {
+      name: "STW",
+      links: [
+        { label: "Noticias", route: "news" },
+        { label: "Servicos", route: "services" },
+        { label: "Eventos", route: "events" },
+        { label: "Estude", route: "study" },
+        { label: "Bolsas", route: "becas" },
+        { label: "Trabalhe", route: "work" },
+      ],
+    },
+    {
+      name: "ESTRANGEIRO",
+      links: [
+        { label: "Destinos", route: "destinations" },
+        { label: "Universidades", route: "universities" },
+        { label: "Academicos", route: "academics" },
+        { label: "Cursos", route: "courses" },
+        { label: "Estude", route: "study" },
+        { label: "Bolsas", route: "becas" },
+        { label: "Trabalhe", route: "work" },
+      ],
+    },
+    {
+      name: "PROJETOS",
+      links: [
+        { label: "Programa_de_Integracao", route: "integration-program" },
+        { label: "Universidades_pelo_Mundo", route: "world-universities" },
+        { label: "Podcasts", route: "podcasts" },
+        { label: "Top_Students", route: "top-students" },
+      ],
+    },
+    {
+      name: "TOPICOS",
+      links: [
+        { label: "Oportunidades", route: "oportunities" },
+        { label: "Tempo", route: "weather" },
+        { label: "Noticias", route: "news" },
+        { label: "Ciencia_e_Inovacao", route: "innovation" },
+      ],
+    },
+  ];
 
   return (
     <>
@@ -34,7 +81,7 @@ export const MenuMobile = () => {
         onClick={() => setDrawer(false)}
         className={"overlay " + (drawer ? " active" : "")}
       ></div>
-      <dialog open={drawer} className="left">
+      <dialog open={drawer} className="">
         <header className="fixed">
           <nav>
             <button
@@ -43,7 +90,41 @@ export const MenuMobile = () => {
             >
               <i>close</i>
             </button>
-            <h5 className="max">Menu Mobile</h5>
+            <div className="max padding left-align">
+              {topics.map((topic) => (
+                <button
+                  className="bold small-padding small-round"
+                  key={topic.name}
+                  onClick={() => {
+                    if (selectedTopic === topic.name) {
+                      setSelectedTopic(null);
+                    } else {
+                      setSelectedTopic(topic.name);
+                    }
+                  }}
+                >
+                  {t(topic.name)}
+                </button>
+              ))}
+            </div>
+            <div>
+              {selectedTopic && (
+                <menu
+                  key={selectedTopic}
+                  className={`min ${
+                    selectedTopic ? "active" : ""
+                  } small-round primary`}
+                >
+                  {topics
+                    .find((topic) => topic.name === selectedTopic)
+                    ?.links.map((link) => (
+                      <Link key={link.route} href={`/${link.route}`}>
+                        {t(link.label)}
+                      </Link>
+                    ))}
+                </menu>
+              )}
+            </div>
           </nav>
         </header>
       </dialog>
