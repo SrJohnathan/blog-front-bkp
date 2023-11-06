7ea23751bc0483e04fe36c7cbcef745ca594e277
DayaneArnaud <dayanearnaud@gmail.com>
2023-11-06 15:15:35 -0300

    Working on integration

--- src/components/Header/NavLinkDesktop/NavLinkDesktop.tsx
+++ src/components/Header/NavLinkDesktop/NavLinkDesktop.tsx
@@ -1,66 +1,64 @@
 "use client";
 
-import {useEffect, useState} from "react";
-import {useLocale, useTranslations} from "next-intl";
+import { useEffect, useState } from "react";
+import { useLocale, useTranslations } from "next-intl";
 import { Collapse } from "./Collapse";
 import Link from "next/link";
-import {Category} from "@/dtos/Category";
-import {Ex} from "@/extension/ex";
-import {getCategoryAll} from "@/source/category";
+import { Category } from "@/dtos/Category";
+import { getCategoryAll } from "@/source/category";
+import React from "react";
 
 export const NavLinkDesktop = () => {
-
-  const [categorys,setC] = useState<Category[]>()
-
-  const  locale = useLocale()
+  const [categoriesData, setCategoriesData] = useState<Category[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const locale = useLocale();
+  const t = useTranslations("NavLinkDesktopFooter");
 
   useEffect(() => {
-    getCategoryAll(locale).then(value =>   {
-      if(value){
-        setC(value)
-      }
-    })
-  }, [setC]);
-
-
-
-  const t = useTranslations("NavLinkDesktopFooter");
-  const [isOpen, setIsOpen] = useState(false);
-
-
+    getCategoryAll(locale).then(setCategoriesData);
+  }, [locale]);
 
   const categories = {
-    STW: [
-       "noticias" ,
-      "servicos" ,
-      "eventos" ,
-     "trabalheconosco" ,
-    ],
+    STW: ["noticias", "servicos", "eventos", "trabalheconosco"],
     ESTRANGEIRO: [
-      { label: "Destinos", route: "destinations" },
-      { label: "Universidades_PT", route: "pt-universities" },
-      { label: "Academicos", route: "academics" },
-      { label: "Cursos", route: "courses" },
-      { label: "Estude", route: "study" },
-      { label: "Bolsas", route: "becas" },
-      { label: "Trabalhe", route: "work" },
+      "destinos",
+      "universidadespt",
+      "academicos",
+      "cursos",
+      "estude",
+      "bolsas",
+      "trabalhe",
     ],
     PROJETOS: [
-      { label: "Programa_de_Integracao", route: "integration-program" },
-      { label: "Universidades_pelo_Mundo", route: "world-universities" },
-      { label: "Podcasts", route: "podcasts" },
-      { label: "Top_Students", route: "top-students" },
+      "programadeintegracao",
+      "universidadespelomundo",
+      "podcasts",
+      "topstudents",
     ],
-    TOPICOS: [
-      { label: "Oportunidades", route: "oportunities" },
-      { label: "Tempo", route: "weather" },
-      { label: "Mais_Noticias", route: "more-news" },
-      { label: "Ciencia_e_Inovacao", route: "innovation" },
-    ],
+    TOPICOS: ["oportunidades", "tempo", "maisnoticias", "cienciaeinovacao"],
   };
 
+  type CategoryKey = keyof typeof categories;
 
-
+  const renderCategorySection = (categoryKey: CategoryKey) => {
+    return (
+      <div key={categoryKey} className={"m3 no-padding"}>
+        <h6 className="small bold">{t(categoryKey)}</h6>
+        <Collapse isOpen={isOpen}>
+          {filter(categoriesData, categories[categoryKey])?.map(
+            (category, index) => (
+              <React.Fragment key={index}>
+                <Link href={`/category/${category.name_url}`}>
+                  <h6 className="small small-line">{category.name}</h6>
+                </Link>
+                <br />
+              </React.Fragment>
+            )
+          )}
+        </Collapse>
+      </div>
+    );
+  };
 
   return (
     <div
@@ -68,91 +66,17 @@ export const NavLinkDesktop = () => {
       onMouseEnter={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
     >
-      {/* Submenu */}
-      <div>
-        <div className={"grid left-align"}>
-
-
-          {["STW"].map((value1, index) =>
-
-              <div key={index}  className={"m3 no-padding"}>
-                <h6 className="small bold">{value1}</h6>
-                <Collapse isOpen={isOpen}>
-                  {filter(categorys,categories.STW)?.map((value, index) =>  {
-                    return (
-                        <>
-                          <Link
-                              key={index}
-                              href={`/category/${value.name_url}`}
-                          >
-                            <h6 className="small small-line">{value.name}</h6>
-                          </Link>
-                          <br/>
-                        </>
-                    )
-                  })}
-                  <div className="space"></div>
-                </Collapse>
-              </div>
-
-
-          )}
-
-
-
-
-
-         {/* {Object.entries(categories).map(([key, links]) => (
-            <div key={key} className={"m3 no-padding"}>
-              <h6 className="small bold">{t(key)}</h6>
-              <Collapse isOpen={isOpen}>
-                {links.map((link, index) => (
-                  <>
-                    <Link
-                      key={`${key}-${index}`}
-                      href={`/${key.toLowerCase()}/${link.route}`}
-                    >
-                      <h6 className="small small-line">{t(link.label)}</h6>
-                    </Link>
-                    <br />
-                  </>
-                ))}
-                <div className="space"></div>
-              </Collapse>
-            </div>
-          ))}*/}
-        </div>
+      <div className={"grid left-align"}>
+        {Object.keys(categories).map((categoryKey) =>
+          renderCategorySection(categoryKey as CategoryKey)
+        )}
       </div>
     </div>
   );
 };
 
-
-
-function filter(ar : Category[]| null | undefined,fi:string[]) {
-
-
-
-
-
-  if(ar){
-     const new_ar =  ar.filter((value, index) =>  {
-
-       if(fi.includes(value.name_url)){
-         return true
-       }else {
-         return  false
-       }
-
-
-     }  )
-
-
-    return new_ar
-  }else {
-    return  null
-  }
-
-
-
+function filter(categories: Category[], filterItems: string[]) {
+  return categories.filter((category) =>
+    filterItems.includes(category.name_url)
+  );
 }
--- src/source/category.ts
+++ src/source/category.ts
@@ -1,50 +1,58 @@
-import {Ex} from "@/extension/ex";
-import {Category, CategoryImp} from "@/dtos/Category";
+import { Ex } from "@/extension/ex";
+import { Category, CategoryImp } from "@/dtos/Category";
 
-
-export async function getCategory(id:string | number,str:string){
-
-    let category : Category =  {active: false, id: 0, name: "",name_url:""}
-    try {
-        const  res :CategoryImp = ( await Ex.apiClient().get(`/api/category/${id}`) ).data
-        switch (str) {
-            case "pt"  : category.name = res.name_pt; break;
-            case "en"  : category.name = res.name_en; break;
-            case "es"  : category.name = res.name_es; break;
-            case "fr"  : category.name = res.name_fr; break;
-        }
-        return  category;
-    }catch (e) {
-        return  null
+export async function getCategory(id: string | number, str: string) {
+  let category: Category = { active: false, id: 0, name: "", name_url: "" };
+  try {
+    const res: CategoryImp = (await Ex.apiClient().get(`/api/category/${id}`))
+      .data;
+    switch (str) {
+      case "pt":
+        category.name = res.name_pt;
+        break;
+      case "en":
+        category.name = res.name_en;
+        break;
+      case "es":
+        category.name = res.name_es;
+        break;
+      case "fr":
+        category.name = res.name_fr;
+        break;
     }
-
+    return category;
+  } catch (e) {
+    return null;
+  }
 }
 
+export async function getCategoryAll(str: string) {
+  const res: CategoryImp[] = (await Ex.apiClient().get(`/api/category/all`))
+    .data;
 
+  const categorys = res.map((value, index) => {
+    let category: Category = {
+      active: true,
+      id: 0,
+      name: "",
+      name_url: value.name_url,
+    };
+    switch (str) {
+      case "pt":
+        category.name = value.name_pt;
+        break;
+      case "en":
+        category.name = value.name_en;
+        break;
+      case "es":
+        category.name = value.name_es;
+        break;
+      case "fr":
+        category.name = value.name_fr;
+        break;
+    }
+    return category;
+  });
 
-export async function getCategoryAll(str:string){
-
-
-    const  res :CategoryImp[] = ( await Ex.apiClient().get(`/api/category/all`) ).data
-
-
-   const categorys = res.map((value, index) => {
-
-
-        let category : Category =  {active: true, id: 0, name: "",name_url:value.name_url}
-            switch (str) {
-                case "pt"  : category.name = value.name_pt; break;
-                case "en"  : category.name = value.name_en; break;
-                case "es"  : category.name = value.name_es; break;
-                case "fr"  : category.name = value.name_fr; break;
-            }
-            return  category;
-
-
-
-
-    })
-
-
-    return categorys
+  return categorys;
 }
