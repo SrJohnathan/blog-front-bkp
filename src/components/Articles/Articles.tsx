5aff32c2ec84361e87de469330af5d8f9b7cd6c1
DayaneArnaud <dayanearnaud@gmail.com>
2023-11-07 15:18:01 -0300

    Working on Articles integration

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -6,11 +6,18 @@ import Link from "next-intl/link";
 import { GetNews } from "@/dtos/News";
 import { useEffect, useState } from "react";
 import { Ex } from "@/extension/ex";
+import { getAllCategories } from "@/source/category";
+import { Category } from "@/dtos/Category";
 
-export const Articles = () => {
+export const Articles = ({ value }: { value: GetNews }) => {
   const t = useTranslations("Ver_Mais");
   const [news, setNews] = useState<GetNews[]>([]);
+  const [categoriesData, setCategoriesData] = useState<Category[]>([]);
   const locale = useLocale();
+
+  useEffect(() => {
+    getAllCategories(locale).then(setCategoriesData);
+  }, [locale]);
 
   useEffect(() => {
     Ex.apiClient()
@@ -22,9 +29,40 @@ export const Articles = () => {
       .catch(() => {});
   }, [locale, setNews]);
 
+  const categories = {
+    TOPICOS: [`${"aqui: value.name_category"}`],
+  };
+
+  type CategoryKey = keyof typeof categories;
+
+  const renderVerMais = (categoryKey: CategoryKey) => {
+    const categoryUrls = categories[categoryKey];
+    const filteredCategoryData = filteredCategories(
+      categoriesData,
+      categoryUrls
+    );
+    if (filteredCategoryData.length > 0) {
+      const category = filteredCategoryData[0];
+      return (
+        <>
+          <Link
+            href={`/category/${category.name_url}`}
+            className={"primary-title margin"}
+          >
+            {t("Ver_Mais")} <i>expand_more</i>{" "}
+          </Link>
+          <br />
+        </>
+      );
+    }
+    return null;
+  };
+
   return (
     <article className={"padding background no-elevate "}>
-      <h6 className={"small bold margin"}>Categoria da API</h6>
+      <h6 className={"small bold margin"}>
+        {"aqui vai o value.name_category"}
+      </h6>
       <div className={"medium-divider"}></div>
       <div className={"small-space"}></div>
       {news.length === 0
@@ -32,9 +70,17 @@ export const Articles = () => {
         : news.map((value, index) => (
             <HorzNewsSqCard value={value} key={index} />
           ))}
-      <Link href={"/"} className={"primary-title margin"}>
-        {t("Ver_Mais")} <i>expand_more</i>
-      </Link>
+      <>
+        {Object.keys(categories).map((categoryKey) =>
+          renderVerMais(categoryKey as CategoryKey)
+        )}
+      </>
     </article>
+  );
+};
+
+const filteredCategories = (categories: Category[], filterItems: string[]) => {
+  return categories.filter((category) =>
+    filterItems.includes(category.name_url)
   );
 };
