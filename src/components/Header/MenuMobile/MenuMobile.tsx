17decfcc850a99e0a67bc9224e1b758a942cc648
DayaneArnaud <dayanearnaud@gmail.com>
2023-11-13 10:59:19 -0300

    Working on Header Mobile

--- src/components/Header/MenuMobile/MenuMobile.tsx
+++ src/components/Header/MenuMobile/MenuMobile.tsx
@@ -10,6 +10,10 @@ import { Category } from "@/dtos/Category";
 import { getAllCategories } from "@/source/category";
 import { useLocale } from "next-intl";
 import { SocialMediaIcons } from "@/components/SocialMediaIcons/SocialMediaIcons";
+import StwIcon from "../../../../public/white-home.svg";
+import AbroadIcon from "../../../../public/white-plane.svg";
+import ProjectsIcon from "../../../../public/white-projects.svg";
+import TopicsIcon from "../../../../public/white-topics.svg";
 
 export const MenuMobile = () => {
   const t = useTranslations("NavLinkDesktopFooter");
@@ -22,89 +26,115 @@ export const MenuMobile = () => {
   }, [locale]);
 
   const categories = {
-    STW: ["noticias", "servicos", "eventos", "trabalheconosco"],
-    ESTRANGEIRO: [
-      "destinos",
-      "universidadespt",
-      "academicos",
-      "cursos",
-      "estude",
-      "bolsas",
-      "trabalhe",
-    ],
-    PROJETOS: [
-      "programadeintegracao",
-      "universidadespelomundo",
-      "podcasts",
-      "topstudents",
-    ],
-    TOPICOS: ["oportunidades", "tempo", "maisnoticias", "cienciaeinovacao"],
+    STW: {
+      topics: ["noticias", "servicos", "eventos", "trabalheconosco"],
+      icon: StwIcon,
+      size: "s2",
+    },
+    ESTRANGEIRO: {
+      topics: [
+        "destinos",
+        "universidadespt",
+        "academicos",
+        "cursos",
+        "estude",
+        "bolsas",
+        "trabalhe",
+      ],
+      icon: AbroadIcon,
+      size: "s4",
+    },
+    PROJETOS: {
+      topics: [
+        "programadeintegracao",
+        "universidadespelomundo",
+        "podcasts",
+        "topstudents",
+      ],
+      icon: ProjectsIcon,
+      size: "s3",
+    },
+    TOPICOS: {
+      topics: ["oportunidades", "tempo", "maisnoticias", "cienciaeinovacao"],
+      icon: TopicsIcon,
+      size: "s3",
+    },
   };
 
-  type CategoryKey = "STW" | "ESTRANGEIRO" | "TOPICOS" | "PROJETOS" | "NULL";
-  const [selectedTopic, setSelectedTopic] = useState<CategoryKey>();
+  type CategoryKey = keyof typeof categories | "NULL";
+  const [selectedTopic, setSelectedTopic] = useState<CategoryKey>("NULL");
 
   return (
     <>
-      <header className={`responsive s primary fixed ${theme}`}>
+      <header className={`small-padding s primary fixed ${theme}`}>
         <div>
           <nav className="grid">
             <Link href={"/"} className="left-align s3">
-              <Image width={80} height={80} src="/stw.svg" alt={"STW icon"} />
+              <Image width={80} height={80} src="/stw.svg" alt="STW icon" />
             </Link>
-
             <div className="s1"></div>
             <div className="s3">
               <SocialMediaIcons />
             </div>
             <div className="s1"></div>
-
             <div className="s2">
               <Languages />
             </div>
-
             <button onClick={toggleTheme} className="circle transparent s2">
               {theme === "dark" ? <i>dark_mode</i> : <i>light_mode</i>}
             </button>
           </nav>
-          <div className="">
+          <div>
             <div className="grid">
-              {Object.keys(categories).map((category, index) => (
-                <h6
-                  style={{
-                    cursor: "pointer",
-                    fontSize: "12px",
-                  }}
-                  onMouseOver={(e) => (e.currentTarget.style.opacity = "0.5")}
-                  onMouseOut={(e) => (e.currentTarget.style.opacity = "1")}
-                  className="small bold s3"
-                  key={index}
-                  onClick={() => {
-                    if (selectedTopic === category) {
-                      setSelectedTopic("NULL");
-                    } else {
-                      setSelectedTopic(category as CategoryKey);
-                    }
-                  }}
-                >
-                  {t(category)}
-                </h6>
-              ))}
-              <div className="small-space"></div>
+              {Object.keys(categories).map((categoryKey) => {
+                const key = categoryKey as keyof typeof categories;
+                const category = categories[key];
+                return (
+                  <div key={categoryKey} className={`${category.size}`}>
+                    <h6
+                      style={{
+                        cursor: "pointer",
+                        fontSize: "12px",
+                        display: "flex",
+                        alignItems: "flex-start",
+                      }}
+                      onMouseOver={(e) =>
+                        (e.currentTarget.style.opacity = "0.5")
+                      }
+                      onMouseOut={(e) => (e.currentTarget.style.opacity = "1")}
+                      onClick={() => {
+                        if (selectedTopic === categoryKey) {
+                          setSelectedTopic("NULL");
+                        } else {
+                          setSelectedTopic(categoryKey as CategoryKey);
+                        }
+                      }}
+                    >
+                      <Image
+                        src={category.icon}
+                        alt={`${categoryKey} icon`}
+                        width={15}
+                        height={15}
+                        style={{
+                          marginRight: "3px",
+                        }}
+                      />
+                      {t(categoryKey)}
+                    </h6>
+                  </div>
+                );
+              })}
             </div>
             <div>
-              {selectedTopic !== "NULL" && (
+              {selectedTopic !== "NULL" && selectedTopic in categories && (
                 <menu
-                  key={selectedTopic}
                   className={`min ${
                     selectedTopic ? "active" : ""
                   } primary bottom-round`}
                 >
                   {filteredCategories(
                     categoriesData,
-                    categories[
-                      selectedTopic === undefined ? "PROJETOS" : selectedTopic
-                    ]
+                    categories[selectedTopic as keyof typeof categories].topics
                   ).map((category, index) => (
                     <Link key={index} href={`/category/${category.name_url}`}>
                       {category.name}
@@ -120,6 +150,7 @@ export const MenuMobile = () => {
     </>
   );
 };
+
 const filteredCategories = (
   categories: Category[],
   filteredItems: string[]
@@ -128,131 +159,3 @@ const filteredCategories = (
     filteredItems.includes(category.name_url)
   );
 };
-
-// "use client";
-
-// import React, { useContext, useEffect, useState } from "react";
-// import { ThemeModeContext } from "@/context/ThemeModeContext";
-// import Image from "next/image";
-// import Link from "next/link";
-// import { useTranslations } from "next-intl";
-// import { Languages } from "../MenuDesktop/Languages/Languages";
-// import { Category } from "@/dtos/Category";
-// import { getAllCategories } from "@/source/category";
-// import { useLocale } from "next-intl";
-
-// export const MenuMobile = () => {
-//   const t = useTranslations("NavLinkDesktopFooter");
-//   const { theme, toggleTheme } = useContext(ThemeModeContext);
-//   const locale = useLocale();
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [categoriesData, setCategoriesData] = useState<Category[]>([]);
-
-//   useEffect(() => {
-//     getAllCategories(locale).then(setCategoriesData);
-//   }, [locale]);
-
-//   const categories = {
-//     STW: ["noticias", "servicos", "eventos", "trabalheconosco"],
-//     ESTRANGEIRO: [
-//       "destinos",
-//       "universidadespt",
-//       "academicos",
-//       "cursos",
-//       "estude",
-//       "bolsas",
-//       "trabalhe",
-//     ],
-//     PROJETOS: [
-//       "programadeintegracao",
-//       "universidadespelomundo",
-//       "podcasts",
-//       "topstudents",
-//     ],
-//     TOPICOS: ["oportunidades", "tempo", "maisnoticias", "cienciaeinovacao"],
-//   };
-
-//   type CategoryKey = "STW" | "ESTRANGEIRO" | "TOPICOS" | "PROJETOS" | "NULL";
-//   const [selectedTopic, setSelectedTopic] = useState<CategoryKey>();
-
-//   return (
-//     <>
-//       <header className={`responsive s primary fixed ${theme}`}>
-//         <nav>
-//           <div className={"absolute left-align"}>
-//             <button
-//               onClick={() => setIsOpen(true)}
-//               className="circle transparent"
-//             >
-//               <i>menu</i>
-//             </button>
-//           </div>
-//           <Link href={"/"} className="center-align max">
-//             <Image width={80} height={80} src="/stw.svg" alt={"STW icon"} />
-//           </Link>
-//           <button onClick={toggleTheme} className="circle transparent">
-//             {theme === "dark" ? <i>dark_mode</i> : <i>light_mode</i>}
-//           </button>
-//         </nav>
-//       </header>
-
-//       <dialog className="left primary" open={isOpen}>
-//         {Object.keys(categories).map((category, index) => (
-//           <button
-//             className="row bold small-padding small-round"
-//             key={index}
-//             onClick={() => {
-//               if (selectedTopic === category) {
-//                 setSelectedTopic("NULL");
-//               } else {
-//                 setSelectedTopic(category as CategoryKey);
-//               }
-//             }}
-//           >
-//             {t(category)}
-//           </button>
-//         ))}
-
-//         <div>
-//           {selectedTopic !== "NULL" && (
-//             <menu
-//               key={selectedTopic}
-//               className={`min ${
-//                 selectedTopic ? "active" : ""
-//               } small-round primary`}
-//             >
-//               {filteredCategories(
-//                 categoriesData,
-//                 categories[
-//                   selectedTopic === undefined ? "PROJETOS" : selectedTopic
-//                 ]
-//               ).map((category, index) => (
-//                 <Link key={index} href={`/category/${category.name_url}`}>
-//                   {category.name}
-//                 </Link>
-//               ))}
-//             </menu>
-//           )}
-//         </div>
-
-//         <div className="right-align">
-
-//           <button
-//             className="transparent circle large"
-//             onClick={() => setIsOpen(false)}
-//           >
-//             <i>close</i>
-//           </button>
-//         </div>
-//       </dialog>
-//     </>
-//   );
-// };
-// const filteredCategories = (
-//   categories: Category[],
-//   filteredItems: string[]
-// ) => {
-//   return categories.filter((category) =>
-//     filteredItems.includes(category.name_url)
-//   );
-// };
