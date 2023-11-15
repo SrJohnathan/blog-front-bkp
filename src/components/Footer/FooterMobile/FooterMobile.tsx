9247eebb4dbc8803621f18978a16baff0bab6820
DayaneArnaud <dayanearnaud@gmail.com>
2023-11-15 11:58:41 -0300

    Working on new footer

--- src/components/Footer/FooterMobile/FooterMobile.tsx
+++ src/components/Footer/FooterMobile/FooterMobile.tsx
@@ -1,125 +1,44 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import { SocialMediaIcons } from "@/components/SocialMediaIcons/SocialMediaIcons";
+import React from "react";
+import { useTranslations } from "next-intl";
+import Link from "next-intl/link";
 import Image from "next/image";
-import { useTranslations, useLocale } from "next-intl";
-import { getAllCategories } from "@/source/category";
-import { Category } from "@/dtos/Category";
-import styles from "./FooterMobile.module.css";
 
 export const FooterMobile = () => {
-  const t = useTranslations("NavLinkDesktopFooter");
-  const secT = useTranslations("Footer");
-  const [categoriesData, setCategoriesData] = useState<Category[]>([]);
-  const locale = useLocale();
-
-  useEffect(() => {
-    getAllCategories(locale).then(setCategoriesData);
-  }, [locale]);
-
-  const categories = {
-    STW: {
-      topics: ["noticias", "servicos", "eventos", "trabalheconosco"],
-    },
-    ESTRANGEIRO: {
-      topics: [
-        "destinos",
-        "universidadespt",
-        "academicos",
-        "cursos",
-        "estude",
-        "bolsas",
-        "trabalhe",
-      ],
-    },
-    PROJETOS: {
-      topics: [
-        "programadeintegracao",
-        "universidadespelomundo",
-        "podcasts",
-        "topstudents",
-      ],
-    },
-    TOPICOS: {
-      topics: ["oportunidades", "tempo", "maisnoticias", "cienciaeinovacao"],
-    },
-  };
-
-  type CategoryKey = keyof typeof categories | "NULL";
-  const [selectedTopic, setSelectedTopic] = useState<CategoryKey>("NULL");
+  const t = useTranslations("Footer");
 
   return (
-    <footer className="responsive s primary">
-      <div className="small-space"></div>
-      <div>
-        <div className={styles.topics}>
-          {Object.keys(categories).map((categoryKey) => {
-            return (
-              <div key={categoryKey} className={styles.category}>
-                <h6
-                  className={styles.title}
-                  onMouseOver={(e) => (e.currentTarget.style.opacity = "0.5")}
-                  onMouseOut={(e) => (e.currentTarget.style.opacity = "1")}
-                  onClick={() => {
-                    if (selectedTopic === categoryKey) {
-                      setSelectedTopic("NULL");
-                    } else {
-                      setSelectedTopic(categoryKey as CategoryKey);
-                    }
-                  }}
-                >
-                  {t(categoryKey)}
-                </h6>
-              </div>
-            );
-          })}
-        </div>
-        <div>
-          {selectedTopic !== "NULL" && selectedTopic in categories && (
-            <menu className={`min ${selectedTopic ? "active" : ""} primary`}>
-              {filteredCategories(
-                categoriesData,
-                categories[selectedTopic as keyof typeof categories].topics
-              ).map((category, index) => (
-                <Link key={index} href={`/category/${category.name_url}`}>
-                  {category.name}
-                </Link>
-              ))}
-              <div className="small-space"></div>
-            </menu>
-          )}
-        </div>
+    <div
+      className="grid s s12 small-padding"
+      style={{ boxShadow: "10px 10px 20px 0px" }}
+    >
+      <div className="s3 center-align small-round">
+        <Link href={"#"} className="vertical">
+          <Image src={"/search.svg"} alt="" width={22} height={22} />
+          <span style={{ color: "#767680" }}>{t("Buscar")}</span>
+        </Link>
       </div>
-      <div className={"grid s12"}>
-        <div className={"s4"}>
-          <Image src={"/stw.svg"} width={100} height={70} alt={""} />
-          <SocialMediaIcons />
-        </div>
-        <div className={"s4"}>
-          <small>{secT("copyrightText")}</small>
-        </div>
-        <div className={"s4"}>
-          <small>{secT("info")}</small>
-        </div>
+      <div className="s3 center-align">
+        <Link href={"#"} className="vertical">
+          <Image src={"/heart.svg"} alt="" width={22} height={22} />
+          <span style={{ color: "#767680" }}>{t("Favorito")}</span>
+        </Link>
       </div>
-      {selectedTopic !== "NULL" && (
-        <div>
-          <div className={"large-space"}></div>
-          <div className={"large-space"}></div>
-          <div className={"large-space"}></div>
-        </div>
-      )}
-      <div className={"large-space"}></div>
-    </footer>
+      <div className="s3 center-align">
+        <Link href={"#"} className="vertical">
+          <Image src={"/bell.svg"} alt="" width={22} height={22} />
+          <span style={{ color: "#767680" }}>{t("Notificacoes")}</span>
+        </Link>
+      </div>
+      <div className="s3 center-align small-round">
+        <Link href={"#"} className="vertical">
+          <Image src={"/profile.svg"} alt="" width={22} height={22} />
+          <span style={{ color: "#767680" }}>{t("Perfil")}</span>
+        </Link>
+      </div>
+    </div>
   );
 };
-const filteredCategories = (
-  categories: Category[],
-  filteredItems: string[]
-) => {
-  return categories.filter((category) =>
-    filteredItems.includes(category.name_url)
-  );
-};
+
+export default FooterMobile;
