e92786a4b269e267cb80a973b98fd5913fb6a83e
DayaneArnaud <dayanearnaud@gmail.com>
2023-11-22 18:49:04 -0300

    Working on Search button

--- src/components/Header/MenuDesktop/MenuDesktop.tsx
+++ src/components/Header/MenuDesktop/MenuDesktop.tsx
@@ -1,61 +1,28 @@
 "use client";
 
-import React, { useContext, useState, useMemo } from "react";
+import React, { useContext, useState } from "react";
 import { ThemeModeContext } from "@/context/ThemeModeContext";
 import { Languages } from "./Languages/Languages";
 import { SocialMediaIcons } from "../../SocialMediaIcons/SocialMediaIcons";
 import Image from "next/image";
 import { NavLinkDesktop } from "../NavLinkDesktop/NavLinkDesktop";
 import Link from "next/link";
-import themeIcon from "../../../../public/theme-icon.svg";
 import SearchModal from "./SearchModal";
-
-const news = [
-  {
-    title: "Novidade em Tecnologia",
-    paragraph:
-      "Descubra as últimas tendências em desenvolvimento de software e como elas podem impactar o futuro.",
-  },
-  {
-    title: "Economia Global",
-    paragraph:
-      "Análise do crescimento econômico em diferentes regiões e seus potenciais efeitos no mercado mundial.",
-  },
-  {
-    title: "Saúde e Bem-estar",
-    paragraph:
-      "Explorando novas técnicas de bem-estar e saúde mental para um estilo de vida mais saudável.",
-  },
-  {
-    title: "Educação e Aprendizado",
-    paragraph:
-      "As melhores práticas para potencializar o aprendizado e desenvolvimento pessoal em qualquer idade.",
-  },
-  {
-    title: "Sustentabilidade e Meio Ambiente",
-    paragraph:
-      "Iniciativas sustentáveis que estão fazendo a diferença no planeta e como você pode contribuir.",
-  },
-];
+import styles from "./SearchModal.module.css";
 
 export const MenuDesktop = () => {
   const { toggleTheme, isDark } = useContext(ThemeModeContext);
   const [search, setSearch] = useState("");
-
-  const filteredNews = useMemo(() => {
-    const lowerSearch = search.toLowerCase();
-    return news.filter(
-      (newsItem) =>
-        newsItem.title.toLowerCase().includes(lowerSearch) ||
-        newsItem.paragraph.toLowerCase().includes(lowerSearch)
-    );
-  }, [search]);
-
   const [showModal, setShowModal] = useState(false);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
     setShowModal(event.target.value.length > 0);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSearch("");
   };
 
   return (
@@ -77,7 +44,7 @@ export const MenuDesktop = () => {
           <div></div>
           <div></div>
 
-          <div className="field label prefix small fill small-round">
+          <div className="field label suffix small-round small border background">
             <input type="text" value={search} onChange={handleSearchChange} />
             <i>search</i>
           </div>
@@ -96,7 +63,124 @@ export const MenuDesktop = () => {
           <div className={"small-space"}></div>
         </div>
       </div>
-      {showModal && <SearchModal posts={filteredNews} />}
+      {showModal && (
+        <div className={styles.backdrop}>
+          <SearchModal search={search} onClose={handleCloseModal} />
+        </div>
+      )}
     </header>
   );
 };
+
+// "use client";
+
+// import React, { useContext, useState, useMemo } from "react";
+// import { ThemeModeContext } from "@/context/ThemeModeContext";
+// import { Languages } from "./Languages/Languages";
+// import { SocialMediaIcons } from "../../SocialMediaIcons/SocialMediaIcons";
+// import Image from "next/image";
+// import { NavLinkDesktop } from "../NavLinkDesktop/NavLinkDesktop";
+// import Link from "next/link";
+// import themeIcon from "../../../../public/theme-icon.svg";
+// import SearchModal from "./SearchModal";
+// import styles from "./SearchModal.module.css";
+
+// const news = [
+//   {
+//     title: "Novidade em Tecnologia",
+//     paragraph:
+//       "Descubra as últimas tendências em desenvolvimento de software e como elas podem impactar o futuro.",
+//   },
+//   {
+//     title: "Economia Global",
+//     paragraph:
+//       "Análise do crescimento econômico em diferentes regiões e seus potenciais efeitos no mercado mundial.",
+//   },
+//   {
+//     title: "Saúde e Bem-estar",
+//     paragraph:
+//       "Explorando novas técnicas de bem-estar e saúde mental para um estilo de vida mais saudável.",
+//   },
+//   {
+//     title: "Educação e Aprendizado",
+//     paragraph:
+//       "As melhores práticas para potencializar o aprendizado e desenvolvimento pessoal em qualquer idade.",
+//   },
+//   {
+//     title: "Sustentabilidade e Meio Ambiente",
+//     paragraph:
+//       "Iniciativas sustentáveis que estão fazendo a diferença no planeta e como você pode contribuir.",
+//   },
+// ];
+
+// export const MenuDesktop = () => {
+//   const { toggleTheme, isDark } = useContext(ThemeModeContext);
+//   const [search, setSearch] = useState("");
+
+//   const filteredNews = useMemo(() => {
+//     const lowerSearch = search.toLowerCase();
+//     return news.filter(
+//       (newsItem) =>
+//         newsItem.title.toLowerCase().includes(lowerSearch) ||
+//         newsItem.paragraph.toLowerCase().includes(lowerSearch)
+//     );
+//   }, [search]);
+
+//   const [showModal, setShowModal] = useState(false);
+
+//   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setSearch(event.target.value);
+//     setShowModal(event.target.value.length > 0);
+//   };
+
+//   const handleCloseModal = () => {
+//     setShowModal(false);
+//     setSearch("");
+//   };
+
+//   return (
+//     <header
+//       className={`responsive m l ${isDark ? "small-blur" : "primary"} fixed`}
+//     >
+//       <div className={"grid"}>
+//         <div className={"m1"}>
+//           <div className={"tiny-space"}></div>
+//           <Link href={"/"} className="left-align transparent">
+//             <Image width={80} height={80} src="/stw_1.png" alt={"STW icon"} />
+//           </Link>
+//           <div className={"tiny-space"}></div>
+//         </div>
+//         <div className="m11 no-padding row">
+//           <div className={"small-space"}></div>
+//           <NavLinkDesktop />
+//           <SocialMediaIcons showShare={false} />
+//           <div></div>
+//           <div></div>
+
+//           <div className="field label suffix small-round small border background">
+//             <input type="text" value={search} onChange={handleSearchChange} />
+//             <i>search</i>
+//           </div>
+
+//           <Languages />
+
+//           <button className="circle transparent" onClick={toggleTheme}>
+//             <Image
+//               src={"/theme-icon.svg"}
+//               alt="Ícone do tema"
+//               width={24}
+//               height={24}
+//             />
+//           </button>
+
+//           <div className={"small-space"}></div>
+//         </div>
+//       </div>
+//       {showModal && (
+//         <div className={styles.backdrop}>
+//           <SearchModal posts={filteredNews} onClose={handleCloseModal} />
+//         </div>
+//       )}
+//     </header>
+//   );
+// };
--- src/components/Header/MenuDesktop/SearchModal.tsx
+++ src/components/Header/MenuDesktop/SearchModal.tsx
@@ -1,23 +1,80 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./SearchModal.module.css";
+import { Ex } from "@/extension/ex";
+import { useLocale } from "next-intl";
+import { GetNews } from "@/dtos/News";
+import Link from "next/link";
 
 interface NewsItem {
-  title: string;
-  paragraph: string;
+  id: number;
+  titulo: string;
+  description: string;
 }
 
 interface SearchModalProps {
-  posts: NewsItem[];
+  onClose: () => void;
+  search: string;
 }
 
-const SearchModal: React.FC<SearchModalProps> = ({ posts }) => {
+const SearchModal: React.FC<SearchModalProps> = ({ onClose, search }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+  const locale = useLocale();
+  const [news, setNews] = useState<GetNews[]>([]);
+
+  const limitTextLength = (text: string, maxLength = 100) => {
+    return text.length > maxLength
+      ? text.substring(0, maxLength) + "..."
+      : text;
+  };
+
+  useEffect(() => {
+    Ex.apiClient()
+      .get(`/api/${locale}/post/list/0/8/desc/all`)
+      .then((response) => {
+        const filteredData = response.data
+          .filter(
+            (post: { titulo: string; description: string }) =>
+              post.titulo.toLowerCase().includes(search.toLowerCase()) ||
+              post.description.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((post: { titulo: string; description: string }) => ({
+            ...post,
+            titulo: post.titulo,
+            description: limitTextLength(post.description),
+          }));
+        setNews(filteredData);
+      })
+      .catch(() => {});
+  }, [locale, setNews, search]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: { target: any }) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} ref={modalRef}>
       <ul>
-        {posts.map((post, index) => (
-          <li key={index}>
-            <h3>{post.title}</h3>
-            <p>{post.paragraph}</p>
+        {news.map((post, index) => (
+          <li key={post.id || index}>
+            <div className="small-space"></div>
+            <Link href={""}>
+              <div className="posts">
+                <h6 className="small bold">{post.titulo}</h6>
+                <p>{post.description}</p>
+              </div>
+            </Link>
+            <div className="small-divider"></div>
           </li>
         ))}
       </ul>
